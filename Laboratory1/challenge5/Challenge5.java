8fbb4d474eda7417b9ac81de884ff1f4c91f30dd
Daniel Ruiz P <edgar.ruiz@mail.escuelaing.edu.co>
2026-08-02 17:36:01 -0500

    Feat: Challenge 5 Part A

--- Laboratory1/challenge5/Challenge5.java
+++ Laboratory1/challenge5/Challenge5.java
@@ -1,7 +1,28 @@
-public class Challenge5 {
-    
-    public static void main(String[] args) {
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+public class Challenge5 {
+
+    public static Set<Integer> battleOfSets() {
+
+        HashSet<Integer> army = new HashSet<>(
+                Arrays.asList(4, 9, 15, 7, 18, 21, 10, 5)
+        );
+
+        return army.stream()
+                .filter(number -> number % 3 != 0)
+                .collect(Collectors.toSet());
     }
 
+    public static void main(String[] args) {
+
+        Set<Integer> result = battleOfSets();
+
+        result.forEach(number ->
+                System.out.println("Number in the arena: " + number)
+        );
+
+    }
 }
