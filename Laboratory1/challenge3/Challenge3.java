aee3ee1a8d63e3c32ec5bf72b4702724a48b46a3
Daniel Ruiz P <edgar.ruiz@mail.escuelaing.edu.co>
2026-08-02 16:01:20 -0500

    Feat: challenge 3 buffer

--- Laboratory1/challenge3/Challenge3.java
+++ Laboratory1/challenge3/Challenge3.java
@@ -1,17 +1,25 @@
 import java.util.function.Function;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Challenge3 {
-    public static Function<String, String> string_builder = str -> {
-        String repeater = Stream.generate(() -> str).limit(3).collect(Collectors.joining(" "));
-        return repeater;
+
+
+    public static Function<String, String> mysteriousEcho = str -> {
+
+        StringBuffer buffer = new StringBuffer(str);
+
+        return buffer.reverse().toString();
     };
 
-    public static void main(String[] args) {
-        String str = "Hello World and cats";
 
-        String result = string_builder.apply(str);
-        System.out.println(result);
+
+    public static void main(String[] args) {
+
+        String message = "Hello World";
+
+        System.out.println(mysteriousEcho.apply(message));
+
     }
+
+
 }
