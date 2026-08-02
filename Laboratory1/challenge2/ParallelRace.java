6ca219b5f955785250ca08f6c40e1d4e3817dbd5
Daniel Santiago Morales Perdomo <daniel.morales-p@mail.escuelaing.edu.co>
2026-08-01 20:17:30 -0500

    feat: Changed the name of the file to ParallelRace and added the base class and data to use.

---
+++ Laboratory1/challenge2/ParallelRace.java
@@ -0,0 +1,23 @@
+public class ParallelRace {
+
+    public static void main(String[] args){
+        List<Integer> nums1 = List.of(32,25,2,5252,5,25,25,25,27,654,7421,86);
+        List<Integer> nums2 = List.of(2,5,2,64,6436,8,-6,9964,0,9589,9,653,98765);
+
+        System.out.println("List 1 stats:");
+        System.out.println("Max: " + "PUT HERE");
+        System.out.println("Min: " + "PUT HERE");
+        System.out.println("Total: " + "PUT HERE");
+        System.out.println("IsMaxMulOfTwo: " + "PUT HERE");
+        System.out.println("IsSizeEven: " + "PUT HERE");
+
+        System.out.println("\n");
+
+        System.out.println("List 2 stats:");
+        System.out.println("Max: " + "PUT HERE");
+        System.out.println("Min: " + "PUT HERE");
+        System.out.println("Total: " + "PUT HERE");
+        System.out.println("IsMaxMulOfTwo: " + "PUT HERE");
+        System.out.println("IsSizeEven: " + "PUT HERE");
+    }
+}
