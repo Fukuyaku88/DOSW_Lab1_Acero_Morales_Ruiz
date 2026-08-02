f3e3978cbdfa4838c9095c70bae05bf2b97d66e2
Miguel Angel Acero Laverde <miguel.acero-l@mail.escuelaing.edu.co>
2026-08-01 20:14:03 -0500

    feat: implement WelcomeMessage class

--- Laboratory1/challenge1/Challenge1.java
+++ Laboratory1/challenge1/Challenge1.java
@@ -1,6 +1,42 @@
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Challenge1 {
 
     public static void main(String[] args) {
  
+    }
+}
+
+class WelcomeMessage {
+    private final List<Student> students;
+
+    public WelcomeMessage(List<Student> students){
+        this.students = students;
+    }
+    public String getCompleteMessage(){
+        int numS = students.size(); // number of students
+
+        if(numS == 0) return "No students registered";
+
+        String paragraphTwo = IntStream.range(0, numS)
+                .mapToObj(i -> {
+                    Student s = students.get(i);
+                    String basicDes = s.getFormattedDetails();
+
+                    if(numS == 1) return basicDes;
+                    if(i == numS-1) return "and " + basicDes;
+                    return basicDes + ",";
+                })
+                .collect(Collectors.joining("\n"));
+
+        String paragraphThree = students.stream()
+                .map(Student::getEmail)
+                .collect(Collectors.joining("\n"));
+
+        return "Hello and welcome \n" +
+                "We are " + paragraphTwo + ".\n\n"
+                + "Our Institutional emails are:" + "\n"
+                + paragraphThree;
     }
 }
--- README.md
+++ README.md
@@ -2,6 +2,23 @@
 **Course:** DOSW (2026-2)  
 **Team Members:** Daniel Santiago Morales Perdomo, Miguel Ángel Acero Laverde, Edgar Daniel Ruiz Patiño
 
+## Initial Config
+
+Miguel Angel Acero Laverde
+
+![Challenge evidence](images/A_0_miguel_%20initial_git_conf.png)
+
+Daniel Santiago Morales Perdomo
+
+![Challenge evidence](images/M_0_rep_creation.png)
+![Challenge evidence](images/M_1_collaborators_added.png)
+![Challenge evidence](images/M_3_morales_initial_git_config.png)
+
+Edgar Daniel Ruiz Patiño
+
+![Challenge evidence](images/R_0_Creacion_develop.png)
+![Challenge evidence](images/R_1_develop_propio.png)
+
 ## Challenge 1 — Welcome Message
 
 ### Evidence
@@ -9,6 +26,12 @@
 ![Challenge evidence](images/challengeN.png)
 
 ### Description
+We had to create a structure message showing basic information
+of each member of the group. Things like: name, currently
+semester and institutional email.
+On the other hand, was necesary create our own
+locally branch to work, for then upload in GitHub and
+merge with develop.
 
 Briefly explain:
 
