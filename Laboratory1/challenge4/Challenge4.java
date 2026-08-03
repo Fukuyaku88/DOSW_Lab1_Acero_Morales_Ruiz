088a71885a7303d0744bfb61a5eeb10845940e58
Daniel Ruiz P <edgar.ruiz@mail.escuelaing.edu.co>
2026-08-02 20:21:32 -0500

    Feat: Student B Hashtable

--- Laboratory1/challenge4/Challenge4.java
+++ Laboratory1/challenge4/Challenge4.java
@@ -16,6 +16,21 @@ public class Challenge4{
                         HashMap::new
                 ));
     }
+    public static Hashtable<String, Integer> treasureDuplicateKeysHtable(List<Map.Entry<String, Integer>> entries) {
+
+        if (entries == null)
+            return new Hashtable<>();
+
+        return entries.stream()
+                .filter(e -> e.getKey() != null && e.getValue() != null)
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (v1, v2) -> v1,
+                        Hashtable::new
+                ));
+    }
+
 
     public static void main(String[] args){
 
@@ -24,6 +39,21 @@ public class Challenge4{
         hashMap.put("silver", 5);
         hashMap.put("ruby", 4);
 
-        System.out.println(treasureDuplicateKeysHmap(new ArrayList<>(hashMap.entrySet())));
+        Hashtable<String, Integer> hashTable = new Hashtable<>();
+        hashTable.put("gold", 15);
+        hashTable.put("emerald", 6);
+        hashTable.put("silver", 8);
+
+        System.out.println("HashMap:");
+        System.out.println(
+                treasureDuplicateKeysHmap(new ArrayList<>(hashMap.entrySet()))
+        );
+
+        System.out.println();
+
+        System.out.println("Hashtable:");
+        System.out.println(
+                treasureDuplicateKeysHtable(new ArrayList<>(hashTable.entrySet()))
+        );
     }
 }
