ac20f186b15d22c91765d833008282f41a3e68bd
Daniel Santiago Morales Perdomo <daniel.morales-p@mail.escuelaing.edu.co>
2026-08-02 20:02:30 -0500

    feat/fix: Challenge 6, added the Student B method, and also make a little improve to the input, this for simplify the tests to the challenge

--- Laboratory1/challenge6/Challenge6.java
+++ Laboratory1/challenge6/Challenge6.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Challenge6 {
 
     //Student A version
@@ -18,11 +24,39 @@ public class Challenge6 {
         }
     }
 
+    //Student B Version
+    private static void executeCommandB(String command, Map<String,Runnable> actions) {
+        Runnable response = actions.get(command);
+        response.run();
+    }
+
     public static void main(String[] args) {
 
-            executeCommandA("GREET");
-            executeCommandA("FAREWELL");
-            executeCommandA("SING");
-            executeCommandA("DANCE");
+        Map<String, Runnable> actions = new HashMap<>(); //Map that contains the Student B commands, is here because it is 
+        actions.put("JOKE", () -> System.out.println("Why did the RAM break up with the CPU? It needed space."));
+        actions.put("SHOUT", () -> System.out.println("STACK OVERFLOW ALERT!"));
+        actions.put("WHISPER", () -> System.out.println("Shhh... the bugs are sleeping."));
+        actions.put("ANALYZE", () -> System.out.println("Processing data... result: You are amazing at programming!"));
+
+        ArrayList<String> commandsA = new ArrayList<>(Arrays.asList("GREET", "FAREWELL", "SING", "DANCE"));
+
+        Scanner scan = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Type the command: ");
+
+            String command = scan.nextLine().trim();
+
+            if (command.equals("EXIT")) {
+                break;
+            }
+            if (commandsA.contains(command)) {
+                executeCommandA(command);
+            } else {
+                executeCommandB(command,actions);
+            }
+        }
+
+        scan.close();
     }
 }
